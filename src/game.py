ff13d4f150ab1328eb7a9a71f73acc8a40e5f4f2
noes <42824671+n0es@users.noreply.github.com>
2023-05-19 14:19:27 -0400

    redoing the main loop to use new classes

--- src/game.py
+++ src/game.py
@@ -1,9 +1,8 @@
 import pygame, sys, math, torch, random
 import numpy as np
 
-from src.neuralnetwork import PPO
-from src.raytracer import RayTracer
-from src.camera import Camera
+from neuralnetwork import PPO
+from raytracer import RayTracer
 from collections import deque
 from pygame.locals import QUIT
 
@@ -62,13 +61,14 @@ class Car:
     self.biases = biases
     self.vel = [0,0]
     self.accel = .2
-    self.speed = 4
+    self.speed = 4.0
     self.width = 6
     self.height = 10
-    self.initial = Car(pos, rot, ID, color, weights, biases)
+    self.rayTracer = RayTracer(pos, rot, 250, (-fov//2, fov//2), fov//tracers)
 
     self.score = 0
-    self.age = 0
+    self.age = 0.0
+    self.time_since_checkpoint = 0.0
 
   def reset(self):
     self = self.initial.copy()
@@ -125,6 +125,23 @@ class Car:
           + time_penalty * self.age
     
     return reward
+  
+  def render(self, screen):
+    sin = math.sin(math.radians(self.rot[0]))
+    cos = math.cos(math.radians(self.rot[0]))
+
+    vertices = [
+      [self.pos[0] + sin*self.height/2 - cos*self.width/2,
+      self.pos[1] + cos*self.height/2 + sin*self.width/2],
+      [self.pos[0] - sin*self.height/2 - cos*self.width/2,
+      self.pos[1] - cos*self.height/2 + sin*self.width/2],
+      [self.pos[0] - sin*self.height/2 + cos*self.width/2,
+      self.pos[1] - cos*self.height/2 - sin*self.width/2],
+      [self.pos[0] + sin*self.height/2 + cos*self.width/2,
+      self.pos[1] + cos*self.height/2 - sin*self.width/2],
+    ]
+
+    pygame.draw.polygon(screen, 'red', vertices)
 
 pi = 3.141592653589793
 score = 0
@@ -132,25 +149,8 @@ FPS = 30
 multiplier = 10
 tracers = 8
 fov = 360
-
+spawn = [200,55]
 width,height = 800,600
-
-
-time_elapsed = time_since_checkpoint = 0
-
-rays = []
-accel = .2
-speed = 4
-keys = []
-startPos = pos = [200,55]
-rot = [-90, 0]
-vel = [0,0]
-
-camera = Camera()
-ray_tracer = RayTracer(pos, rot, 250, (-fov//2, fov//2), fov//tracers)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-nn = PPO(11, 64, 5, device)
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
@@ -158,200 +158,118 @@ pygame.display.set_caption('vroom!')
 clock = pygame.time.Clock()
 font = pygame.font.SysFont('Arial', 10)
 
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# nn = PPO(11, 64, 5, device)
+
 track = Track('tracks/new.track')
 
+epoch = 0
+cars = np.empty(10, dtype = Car)
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT: running = False
+  pygame.draw.rect(screen, (255,255,255), pygame.Rect(0, 0, width, height))
+  tick = clock.tick(FPS)
+  dt = tick / (1000/(FPS*multiplier))
 
-def preprocess_experiences(states, actions, rewards, next_states, dones, gamma=0.99, lambd=0.95):
-    values = nn.value_function(torch.tensor(states, dtype=torch.float32).to(device)).cpu().detach().numpy().squeeze()
-    next_values = nn.value_function(torch.tensor(next_states, dtype=torch.float32).to(device)).cpu().detach().numpy().squeeze()
-    td_errors = rewards + (1 - dones) * gamma * next_values - values
-    advantages = []
-    gae = 0
-    for delta_t in reversed(td_errors):
-        gae = delta_t + gamma * lambd * gae
-        advantages.insert(0, gae)
-    returns = advantages + values
-    return np.array(states), np.array(actions), np.array(advantages), np.array(returns)
+  
 
-def collect_experiences():
-    global pos, rot, vel, score, time_elapsed, time_since_checkpoint, ray_tracer, nn
-    experiences = []
-    while len(experiences) < EXPERIENCES_PER_UPDATE:
-        
-        pygame.draw.rect(screen, (255,255,255), pygame.Rect(0, 0, width, height))
-        tick = clock.tick(120)
-        dt = tick / (1000/(FPS*multiplier))
-        time_since_checkpoint += (tick/1000) * multiplier
-        time_elapsed += (tick/1000) * multiplier
-        for event in pygame.event.get():
-          if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-          if event.type == pygame.KEYDOWN:
-            keys.append(event.key)
-          if event.type == pygame.KEYUP:
-            keys.remove(event.key)
 
-        # Update and display RayTracer
-        ray_tracer.update()
-        # track is a list of lines. lines look like [x1,y1,x2,y2]
-        ray_tracer.display(screen,track.walls,camera)
+  pygame.display.update()
 
-        for i in keys:
-          match i:
-            case pygame.K_w:
-              vel[0] -= math.sin(math.radians(rot[0]))*accel*dt
-              vel[1] -= math.cos(math.radians(rot[0]))*accel*dt
-            case pygame.K_s:
-              vel[0] += math.sin(math.radians(rot[0]))*accel*dt
-              vel[1] += math.cos(math.radians(rot[0]))*accel*dt
-            case pygame.K_a:
-              rot[0] += .3*dt
-            case pygame.K_d:
-              rot[0] -= .3*dt
-            case pygame.K_SPACE:
-              vel[0] = vel[0]*.9
-              vel[1] = vel[1]*.9
+'''
+time_since_checkpoint += (tick/1000) * multiplier
+time_elapsed += (tick/1000) * multiplier
+for event in pygame.event.get():
+  if event.type == QUIT:
+    pygame.quit()
+    sys.exit()
+  if event.type == pygame.KEYDOWN:
+    keys.append(event.key)
+  if event.type == pygame.KEYUP:
+    keys.remove(event.key)
 
-        # Collect input data for the neural network
-        nn_input = []
-        for i in ray_tracer.distances:
-          nn_input.append(i)
-        nn_input.append(vel[0])
-        nn_input.append(vel[1])
-        nn_input.append(rot[0])
+# Update and display RayTracer
+ray_tracer.update()
+# track is a list of lines. lines look like [x1,y1,x2,y2]
+ray_tracer.display(screen,track.walls)
 
-        # Predict the output using the neural network
-        predicted_output = nn.calculate_outputs(nn_input)
+# Collect input data for the neural network
+nn_input = []
+for i in ray_tracer.distances:
+  nn_input.append(i)
+nn_input.append(vel[0])
+nn_input.append(vel[1])
+nn_input.append(rot[0])
 
-        # Map the predicted output to actions
-        action = choose_action(predicted_output)
+# Predict the output using the neural network
+predicted_output = nn.calculate_outputs(nn_input)
 
-        # Perform the corresponding action
-        if action == 0:  # Move forward
-          vel[0] -= math.sin(math.radians(rot[0]))*accel*dt
-          vel[1] -= math.cos(math.radians(rot[0]))*accel*dt
-        elif action == 1:  # Turn left
-          rot[0] += .8*dt
-        elif action == 2:  # Turn right
-          rot[0] -= .8*dt
-        elif action == 3: # Brake
-          vel[0] = vel[0]*.9
-          vel[1] = vel[1]*.9
+# Map the predicted output to actions
+action = choose_action(predicted_output)
 
-        vel = cap_velocity(vel,3)
-        pos[0]+=vel[0]*dt
-        pos[1]+=vel[1]*dt
-        #vel = [*map(lambda i:.99*i if abs(i)>.1 else 0, vel),]
-        # this shit caused issues. idk. friction no worky
+# Perform the corresponding action
+if action == 0:  # Move forward
+  vel[0] -= math.sin(math.radians(rot[0]))*accel*dt
+  vel[1] -= math.cos(math.radians(rot[0]))*accel*dt
+elif action == 1:  # Turn left
+  rot[0] += .8*dt
+elif action == 2:  # Turn right
+  rot[0] -= .8*dt
+elif action == 3: # Brake
+  vel[0] = vel[0]*.9
+  vel[1] = vel[1]*.9
 
-        sin = math.sin(math.radians(rot[0]))
-        cos = math.cos(math.radians(rot[0]))
-        cheight = 10
-        cwidth = 6
-        vertices = [
-          [pos[0]-camera.pos[0] + sin*cheight/2 - cos*cwidth/2,
-          pos[1]-camera.pos[1] + cos*cheight/2 + sin*cwidth/2],
-          [pos[0]-camera.pos[0] - sin*cheight/2 - cos*cwidth/2,
-          pos[1]-camera.pos[1] - cos*cheight/2 + sin*cwidth/2],
-          [pos[0]-camera.pos[0] - sin*cheight/2 + cos*cwidth/2,
-          pos[1]-camera.pos[1] - cos*cheight/2 - sin*cwidth/2],
-          [pos[0]-camera.pos[0] + sin*cheight/2 + cos*cwidth/2,
-          pos[1]-camera.pos[1] + cos*cheight/2 - sin*cwidth/2],
-        ]
+vel = cap_velocity(vel,3)
+pos[0]+=vel[0]*dt
+pos[1]+=vel[1]*dt
+#vel = [*map(lambda i:.99*i if abs(i)>.1 else 0, vel),]
+# this shit caused issues. idk. friction no worky
 
-        # lag the camera behind the car
-        camera.pos[0] = (((pos[0]-width/2) - camera.pos[0]) / 2) + camera.pos[0]
-        camera.pos[1] = (((pos[1]-height/2) - camera.pos[1]) / 2) + camera.pos[1]
-        camera.rot = ((rot[0]-camera.rot)/1.3)+camera.rot
 
-        pygame.draw.polygon(screen, 'red', vertices)
-        # for i in range(len(vertices)):
-        #   vertices[i][0]+=camera.pos[0]
-        #   vertices[i][1]+=camera.pos[1]
+coords = font.render(f'[{pos[0]//1},{pos[1]//1}], {rot[0]}*', True, (255, 0, 0))
+screen.blit(coords, (5, 5))
+# draw track lines
+for line in track.walls:
+  pygame.draw.line(screen, 'red', (line[0], line[1]),
+                    (line[2], line[3]), 2)
+# draw checkpoint lines
+for i,line in enumerate(track.checkpoints):
+  pygame.draw.line(screen,'blue', (line[0], line[1]),
+                    (line[2], line[3]), 2)
+  # draw checkpoint number
+  num = font.render(str(i), True, (0, 0, 0))
+  screen.blit(num, ((line[0] + line[2]) / 2, (line[1] + line[3]) / 2))
 
-        coords = font.render(f'[{pos[0]//1},{pos[1]//1}], {rot[0]}*', True, (255, 0, 0))
-        screen.blit(coords, (5, 5))
-        # draw track lines
-        for line in track.walls:
-          pygame.draw.line(screen, 'red', (line[0] - camera.pos[0], line[1] - camera.pos[1]),
-                            (line[2] - camera.pos[0], line[3] - camera.pos[1]), 2)
-        # draw checkpoint lines
-        for i,line in enumerate(track.checkpoints):
-          pygame.draw.line(screen,'blue', (line[0] - camera.pos[0], line[1] - camera.pos[1]),
-                            (line[2] - camera.pos[0], line[3] - camera.pos[1]), 2)
-          # draw checkpoint number
-          num = font.render(str(i), True, (0, 0, 0))
-          screen.blit(num, ((line[0] + line[2]) / 2 - camera.pos[0], (line[1] + line[3]) / 2 - camera.pos[1]))
-        
-          # Added check for passing the checkpoint and increase the score
-        if len(track.checkpoints) > 0:
-          if detect_collision(vertices, [track.checkpoints[score % len(track.checkpoints)]], camera):
-            score += 1
-            time_since_checkpoint = 0
+  # Added check for passing the checkpoint and increase the score
+if len(track.checkpoints) > 0:
+  if detect_collision(vertices, [track.checkpoints[score % len(track.checkpoints)]]):
+    score += 1
+    time_since_checkpoint = 0
 
-          # Check for collision and reset the car if there's a collision
-        if detect_collision(vertices, track.walls, camera) or time_since_checkpoint >= 15:
-          time_elapsed = time_since_checkpoint = 0 
-          pos, rot, vel, nn = reset_car(pos, rot, vel, [11, 9, 5])
-          ray_tracer = RayTracer(pos, rot, 250, (-fov//2, fov//2), fov//tracers)
+  # Check for collision and reset the car if there's a collision
+if detect_collision(vertices, track.walls) or time_since_checkpoint >= 15:
+  time_elapsed = time_since_checkpoint = 0 
+  pos, rot, vel, nn = reset_car(pos, rot, vel, [11, 9, 5])
+  ray_tracer = RayTracer(pos, rot, 250, (-fov//2, fov//2), fov//tracers)
 
-          reward = calculate_reward(score, time_elapsed)
-          done = True
-          experiences.append((nn_input, action, reward, nn_input, float(done)))
-          continue
+  reward = 0
 
-        # Score display
-        score_text = font.render(f'Score: {score}', True, (0, 0, 0))
-        screen.blit(score_text, (width - 70, 5))
-        time_text = font.render(str(time_since_checkpoint), True, (0, 0, 0))
-        screen.blit(time_text, (width - 70, 15))
+# Score display
+score_text = font.render(f'Score: {score}', True, (0, 0, 0))
+screen.blit(score_text, (width - 70, 5))
+time_text = font.render(str(time_since_checkpoint), True, (0, 0, 0))
+screen.blit(time_text, (width - 70, 15))
 
-        s = font.render(str((vel[0]**2+vel[1]**2)**.5), True, 'blue')
-        screen.blit(s, (5,15))
-        for i,d in enumerate(ray_tracer.distances):
-          t = font.render(str(round(d,2)), True, 'blue')
-          screen.blit(t, (5,25+i*10))
+s = font.render(str((vel[0]**2+vel[1]**2)**.5), True, 'blue')
+screen.blit(s, (5,15))
+for i,d in enumerate(ray_tracer.distances):
+  t = font.render(str(round(d,2)), True, 'blue')
+  screen.blit(t, (5,25+i*10))
 
-        # Draw velocity vector
-        velocity_scale = 20  # Adjust this value to change the length of the velocity vector
-        velocity_vector = (pos[0]-camera.pos[0] + vel[0] * velocity_scale, pos[1]-camera.pos[1] + vel[1] * velocity_scale)
-        pygame.draw.line(screen, (0, 0, 255), (pos[0]-camera.pos[0],pos[1]-camera.pos[1]), velocity_vector, 2)
-          
-        pygame.display.update()
-        if collect_experiences_flag:
-          done = detect_collision(vertices, track.walls, camera) or time_since_checkpoint >= 15
-          reward = calculate_reward(score, time_since_checkpoint)
-          nn_input_next = nn_input[:]
-
-          experiences.append((nn_input, action, reward, nn_input_next, float(done)))
-        
-        if done:
-            pos, rot, vel, nn = reset_car(pos, rot, vel, [11, 64, 5])
-            ray_tracer = RayTracer(pos, rot, 250, (-fov // 2, fov // 2), fov // tracers)
-            time_elapsed = time_since_checkpoint = 0
-            score = 0
-
-    return experiences
-
-EXPERIENCES_PER_UPDATE = 3072
-UPDATES_PER_ITERATION = 10
-ITERATIONS = 50
-collect_experiences_flag = False
-
-iteration = 0
-while iteration < ITERATIONS:
-    collect_experiences_flag = True
-    step_rewards = 0
-
-    experiences = collect_experiences()
-    states, actions, rewards, next_states, dones = zip(*experiences)
-    states, actions, advantages, returns = preprocess_experiences(states, actions, rewards, next_states, dones)
-
-    for _ in range(UPDATES_PER_ITERATION):
-        log_probs_old = nn.pol_eval(torch.tensor(states, dtype=torch.float32).to(device)).gather(
-            1, torch.tensor(actions, dtype=torch.int64).unsqueeze(1).to(device)).log().cpu().detach().numpy()
-        nn.update(states, actions, log_probs_old.squeeze(), returns, advantages)
-
-    collect_experiences_flag = False
-    iteration += 1
+# Draw velocity vector
+velocity_scale = 20  # Adjust this value to change the length of the velocity vector
+velocity_vector = (pos[0] + vel[0] * velocity_scale, pos[1] + vel[1] * velocity_scale)
+pygame.draw.line(screen, (0, 0, 255), (pos[0],pos[1]), velocity_vector, 2)
+  '''
