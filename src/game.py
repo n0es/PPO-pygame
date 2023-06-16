8c259bd7794ee688392dc388fc5438770ce541df
Ethan White <ethan@upperlevelcrm.com>
2023-06-16 09:14:22 -0400

    testing new algos

--- src/game.py
+++ src/game.py
@@ -1,8 +1,8 @@
-import pygame, sys, math, torch, random, os
+import pygame, sys, math, torch, random, os, shutil
 import numpy as np
 import pickle
+from torch import nn
 
-from neuralnetwork import PPO
 from raytracer import RayTracer
 from pygame.locals import QUIT
 
@@ -14,19 +14,80 @@ def save_model(model, generation, save_path='./saves', file_name_prefix='best_ca
 
   print(f"Model saved at {file_path}")
 
-def mutate_weights_biases(parent_model, mutation_rate=0.1, mutation_scale=0.5):
-    parent_weights = parent_model.get_weights()
-    parent_biases = parent_model.get_biases()
+def load_initial_model(file_name):
+  with open(file_name, 'rb') as file:
+    initial_model = pickle.load(file)
+  return initial_model
 
-    def mutate_array(arr):
-        mutation_mask = np.random.rand(*arr.shape) < mutation_rate
-        mutation_values = np.random.uniform(-mutation_scale, mutation_scale, arr.shape)
-        return arr + (mutation_mask * mutation_values)
+def convert_to_float32(module):
+  for child in module.children():
+    convert_to_float32(child)
+  for param in module.parameters():
+    param.data = param.data.float()
+    param.requires_grad = True
 
-    mutated_weights = [mutate_array(w) for w in parent_weights]
-    mutated_biases = [mutate_array(b) for b in parent_biases]
+def mutate_model(parent_model, parent_fitness=0.0, mutation_scale=0.5):
+  # Determine dynamic mutation rate
+  def calculate_mutation_rate(parent_fitness):
+    return max(0.1, parent_fitness / 10)
 
-    return mutated_weights, mutated_biases
+  # Apply mutation to a weight matrix
+  def mutate_array(arr, mutation_rate, mutation_scale):
+    mutation_mask = np.random.rand(*arr.shape) < mutation_rate
+    mutation_values = np.random.uniform(-mutation_scale, mutation_scale, arr.shape)
+    return arr + (mutation_mask * mutation_values)
+
+  mutation_rate = calculate_mutation_rate(parent_fitness)
+
+  parent_weights = [p.data.numpy() for p in parent_model.parameters() if p.requires_grad]
+  mutated_weights = [mutate_array(w, mutation_rate, mutation_scale) for w in parent_weights]
+  mutation_tensors = [torch.tensor(w, requires_grad=True) for w in mutated_weights]
+
+  for i, tensor in enumerate(parent_model.parameters()):
+    if tensor.requires_grad:
+      tensor.data = mutation_tensors.pop(0)
+
+  return parent_model
+
+def create_new_generation(car_scores, num_best_parents=2, elitism=2, population_size=10):
+  sorted_cars_by_score = [car_score[0] for car_score in sorted(car_scores, key=lambda x: x[1], reverse=True)]
+  sorted_normalized_scores = np.array([normalized_score for _, normalized_score in sorted(car_scores, key=lambda x: x[1], reverse=True)])
+  for i in range(len(sorted_normalized_scores)):
+    sorted_normalized_scores[i] += abs(sorted_normalized_scores[-1])
+
+  total_scores = np.sum(sorted_normalized_scores)
+  probability_dist = sorted_normalized_scores / total_scores
+
+  best_parents = sorted_cars_by_score[:num_best_parents]
+
+  new_generation = best_parents[:elitism].copy()
+
+  for _ in range(population_size - elitism):
+    parent1, parent2 = np.random.choice(sorted_cars_by_score, size=2, p=probability_dist)
+
+    # Crossover parent weights
+    child_weights = []
+    parent1_weights = [p.data.numpy() for p in parent1.nn.parameters() if p.requires_grad]
+    parent2_weights = [p.data.numpy() for p in parent2.nn.parameters() if p.requires_grad]
+
+    for parent1_w, parent2_w in zip(parent1_weights, parent2_weights):
+      crossover_mask = np.random.randint(0, 2, parent1_w.shape).astype(bool)
+      child_weight = np.where(crossover_mask, parent1_w, parent2_w)
+      child_weights.append(child_weight)
+
+    new_car = Car(pos=spawn, rot=-90)
+    child_nn = mutate_model(parent1.nn)
+
+    # Replace child NN weights with crossover weights
+    for idx, tensor in enumerate(child_nn.parameters()):
+      if tensor.requires_grad:
+        tensor.data = torch.tensor(child_weights[idx], requires_grad=True)
+
+    new_car.nn = child_nn
+    new_generation.append(new_car)
+
+  return new_generation
+
 
 class Track:
   def __init__(self, file_name):
@@ -87,7 +148,9 @@ class Line:
     return False
 
 class Car: 
-  def __init__(self, pos = [300,140], rot=-90, ID='0', color='#ff0000', weights=[], biases=[]):
+  def __init__(self, pos=None, rot=None, ID='0', color='#ff0000', weights=[], biases=[]):
+    pos = pos or [300,140]
+    rot = rot or -90
     self.dead = False    # [200,75]
     self.pos = pos
     self.rot = rot
@@ -100,6 +163,9 @@ class Car:
     self.width = 6
     self.height = 10
     self.rayTracer = RayTracer(pos, rot, 250, (-fov//2, fov//2), fov//tracers)
+    self.best_score = 0
+    self.best_weights = None
+    self.best_biases = None
     self.initial = {
       'pos': pos,
       'rot': rot,
@@ -117,7 +183,12 @@ class Car:
       }
 
     }
-    self.nn = PPO(11, 32, 5)
+    self.nn = nn.Sequential(
+            nn.Linear(11, 32),
+            nn.ReLU(),
+            nn.Linear(32, 5),
+            nn.Sigmoid()
+        )
 
     self.score = 0
     self.age = 0.0
@@ -128,9 +199,9 @@ class Car:
     self.score = 0
     self.age = 0.0
     self.time_since_checkpoint = 0.0
-    self.pos = self.initial['pos'][:]
+    self.pos = self.initial['pos'].copy()
     self.rot = self.initial['rot']
-    self.vel = self.initial['vel'][:]
+    self.vel = self.initial['vel'].copy()
   
   def intersects(self, line):
     vertices = self.vertices()
@@ -161,6 +232,7 @@ class Car:
     return False
 
   def choose_action(self, action_probs):
+    action_probs = action_probs.squeeze()
     arr = [np.random.choice(2, p=[1-prob, prob]) for prob in action_probs]
     return int(''.join([str(i) for i in arr]), 2)
   
@@ -179,25 +251,48 @@ class Car:
     return False
 
   def calculate_reward(self):
-    checkpoint_reward = 20
-    time_penalty = 2.5
-    death_pentalty = 30
+    checkpoint_reward = 50
+    time_penalty = 5
+    death_pentalty = 5
     speed_penalty_weight = 5
 
     speed = math.sqrt(self.vel[0]**2 + self.vel[1]**2)
     min_speed_threshold = 1
     speed_penalty = 0 if speed >= min_speed_threshold else abs(min_speed_threshold - speed) * speed_penalty_weight
 
-    reward = (checkpoint_reward * self.score) / (self.age + time_penalty) - (death_pentalty if self.dead else 0) - speed_penalty
+    reward = (checkpoint_reward * self.score**2) / (self.age + time_penalty) - (death_pentalty if self.dead else 0) - speed_penalty
     
     return reward
   
+  def get_reward_color(self):
+    reward = self.calculate_reward()
+
+    def clamp(value, minimum, maximum):
+      return max(minimum, min(value, maximum))
+
+    if reward >= 0:
+      green_value = clamp(int((reward / 40) * 255), 0, 255)
+      color = (255 - green_value, 255, 0)
+    else:
+      red_value = clamp(int(((abs(reward)) / 40) * 255), 0, 255)
+      color = (255, 255 - red_value, 0)
+
+    return color
+
+  
   def render(self, screen):
     self.rayTracer.display(screen,track.walls)
-    pygame.draw.polygon(screen, self.color, car.vertices())
-    
+    color = self.get_reward_color()
+    pygame.draw.polygon(screen, color, car.vertices())
+
+    # Render car's reward as text
+    car_reward = round(self.calculate_reward(), 2)
+    reward_text = font.render(str(car_reward), True, (0, 0, 0))
+    screen.blit(reward_text, (self.pos[0] - 10, self.pos[1] - 10))
 
   def update(self, tick):
+    if self.dead:
+      return
     dt = tick/1000
     self.age += dt
     self.time_since_checkpoint += dt
@@ -206,9 +301,10 @@ class Car:
     self.rayTracer.pos = self.pos
     self.rayTracer.rot = self.rot
 
+    convert_to_float32(self.nn)
     nn_input = self.rayTracer.distances + self.vel + [self.rot]
-    nn_input = torch.tensor(nn_input, dtype=torch.float32, device=(torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))).clone().detach()
-    predicted_output = self.nn.calculate_outputs(nn_input)
+    nn_input = torch.tensor(nn_input, dtype=torch.float32, device=(torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))).unsqueeze(0)
+    predicted_output = self.nn(nn_input).detach().cpu().numpy()
     action = self.choose_action(predicted_output)
 
     sin_rot = math.sin(math.radians(self.rot))
@@ -258,15 +354,26 @@ pygame.display.set_caption('vroom!')
 clock = pygame.time.Clock()
 font = pygame.font.SysFont('Arial', 10)
 
+initial_model_file = None # os.path.join(os.path.dirname(__file__), "../saves/best_car_generation_1000.pkl")
 track_file = os.path.join(os.path.dirname(__file__), '../tracks/new.track')
 track = Track(track_file)
 
+if initial_model_file:
+  initial_model = load_initial_model(initial_model_file)
+else:
+  initial_model = nn.Sequential(
+            nn.Linear(11, 32),
+            nn.ReLU(),
+            nn.Linear(32, 5),
+            nn.Sigmoid()
+        )
+
 population_size = 10
 cars = [Car() for _ in range(population_size)]
-for car in cars:
-  new_weights, new_biases = mutate_weights_biases(car.nn)
-  car.nn.set_weights(new_weights)
-  car.nn.set_biases(new_biases) 
+for i, car in enumerate(cars):
+  car.nn = initial_model
+  if not (i == 0 and initial_model):
+    car.nn = mutate_model(car.nn)
 car_scores = []
 
 generations = 1000
@@ -275,7 +382,11 @@ elitism = 2
 
 for generation in range(generations):
     print(f"Generation {generation + 1}")
-
+    for car in cars:
+      car.pos = spawn
+      car.rot = -90
+      car.vel = [0,0]
+      car.dead = False
     running = True
     while running:
       tick = clock.tick(FPS)
@@ -291,11 +402,10 @@ for generation in range(generations):
 
         if car.collides(track.walls) or car.time_since_checkpoint > 15:
           car.dead = True
-          car_scores.append((car, car.calculate_reward()))
-          cars.pop(i)
-        if len(cars) <= num_best_parents:
+          car.vel = [0,0]
+
+        if all(car.dead for car in cars):
           running = False
-          break
 
       # frame rate monitor
       fps = font.render(str(int(clock.get_fps())), True, pygame.Color('Red'))
@@ -306,41 +416,23 @@ for generation in range(generations):
 
       pygame.display.flip()
 
-    # Add cars that are still running
-    car_scores.extend([(car, car.calculate_reward()) for car in cars])
+    car_scores = []
+    for car in cars:
+      car_scores.append((car, car.calculate_reward()))
 
-    # Sort cars based on their scores
-    car_scores.sort(key=lambda x: x[1], reverse=True)
+    print(len(car_scores))
+    new_generation = create_new_generation(car_scores, num_best_parents, elitism, population_size)
 
-    # Update best_parents
-    best_parents = [car_score[0] for car_score in car_scores[:num_best_parents]]
-
-    # Normalize rewards
-    rewards = [score for _, score in car_scores]
-    mean_reward = np.mean(rewards)
-    std_reward = np.std(rewards)
-    normalized_rewards = [(score - mean_reward) / std_reward for score in rewards]
-    
-    # Create the new generation of cars with mutated weights/biases
-    new_generation = []
-
-    for i in range(elitism):
-      new_car = Car(pos=spawn, rot=-90)
-      new_car.nn = best_parents[i].nn
-      new_generation.append(new_car)
-    
-    for i in range(population_size - elitism):
-      parent_index = int(best_parents[i % num_best_parents].ID)
-      new_weights, new_biases = mutate_weights_biases(cars[parent_index].nn)
-      new_car = Car(pos=spawn, rot=-90)
-      new_car.nn.set_weights(new_weights)
-      new_car.nn.set_biases(new_biases)
-
-      new_generation.append(new_car)
+    best_parents = sorted(car_scores, key=lambda x: x[1], reverse=True)[:num_best_parents]
 
     if (generation + 1) % 25 == 0:
       best_car = best_parents[0]
       save_model(best_car.nn, generation + 1)
+
+    # Update the cars and models with the new generation
+    cars = new_generation
+    for car in cars:
+      car.reset()
 
     # Update the cars and models with the new generation
     cars = new_generation
